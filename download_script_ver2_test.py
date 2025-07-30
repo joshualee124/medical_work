163c60e4a102ba00617eaf1c72a8958e9f12f114
joshualee124 <joshua.ks.lee09@gmail.com>
2025-07-30 01:15:26 -0400

    brain mri ver 2

--- download_script_ver2_test.py
+++ download_script_ver2_test.py
@@ -5,7 +5,7 @@ import json
 import os
 import time
 
-DOWNLOAD_DIR = "datasets/knee_mri_test"
+DOWNLOAD_DIR = "datasets/brain_mri_png"
 BASE_IMG_URL = "https://openi.nlm.nih.gov"
 RETRY_LIMIT = 5
 INITIAL_RETRY_DELAY = 2
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     import requests
 
     def get_img_json(query, m=1, n=10, vid=0):
-        url = f"https://openi.nlm.nih.gov/api/search?m={m}&n={n}&query={query}&vid={vid}&imgType=graphics"
+        url = f"https://openi.nlm.nih.gov/api/search?m={m}&n={n}&query={query}&vid={vid}&it=xg"
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -81,7 +81,7 @@ if __name__ == "__main__":
             return {"list": []}
 
     api_request_interval = 20   
-    total_images_to_process = 100  # adjust as needed
+    total_images_to_process = 43735  # adjust as needed
     delay_between_api_calls = 5
 
     for i in range(0, total_images_to_process, api_request_interval):
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         n = i + api_request_interval
 
         # change your search query here
-        list_of_json_objects_batch = get_img_json("knee mri", m=m, n=n, vid=0)
+        list_of_json_objects_batch = get_img_json("brain mri", m=m, n=n, vid=0)
 
         if 'list' in list_of_json_objects_batch and list_of_json_objects_batch['list']:
             print(f"\nProcessing {len(list_of_json_objects_batch['list'])} images ({m}-{n})")
