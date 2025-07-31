2650560cb756d631f5f6f0fee5ad9440260d9079
joshualee124 <joshua.ks.lee09@gmail.com>
2025-07-30 22:19:15 -0400

    orbit ct  ver 2

--- download_script_ver2_test.py
+++ download_script_ver2_test.py
@@ -5,10 +5,10 @@ import json
 import os
 import time
 
-DOWNLOAD_DIR = "datasets/orbit_xray_png"
+DOWNLOAD_DIR = "datasets/orbit_ct_png"
 BASE_IMG_URL = "https://openi.nlm.nih.gov"
 RETRY_LIMIT = 5
-INITIAL_RETRY_DELAY = 2
+INITIAL_RETRY_DELAY = 2 
 
 async def download_image_from_json(session, json_obj, current_DOWNLOAD_DIR, retries=0):
     """
@@ -81,7 +81,7 @@ if __name__ == "__main__":
             return {"list": []}
 
     api_request_interval = 20   
-    total_images_to_process = 1005  # adjust as needed
+    total_images_to_process = 2427  # adjust as needed
     delay_between_api_calls = 5
 
     for i in range(0, total_images_to_process, api_request_interval):
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         n = i + api_request_interval    
 
         # change your search query here
-        list_of_json_objects_batch = get_img_json("orbit xray", m=m, n=n, vid=0)
+        list_of_json_objects_batch = get_img_json("orbit ct", m=m, n=n, vid=0)
 
         if 'list' in list_of_json_objects_batch and list_of_json_objects_batch['list']:
             print(f"\nProcessing {len(list_of_json_objects_batch['list'])} images ({m}-{n})")
