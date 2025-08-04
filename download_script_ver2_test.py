f5f7499fbe8b14bfb8a0f96be446859456e30317
joshualee124 <joshua.ks.lee09@gmail.com>
2025-08-03 20:13:10 -0400

    shoulder xray png ver 2

--- download_script_ver2_test.py
+++ download_script_ver2_test.py
@@ -5,7 +5,7 @@ import json
 import os
 import time
 
-DOWNLOAD_DIR = "datasets/pelvis_mri_png"            
+DOWNLOAD_DIR = "datasets/shoulder_xray_png"            
 BASE_IMG_URL = "https://openi.nlm.nih.gov"  
 RETRY_LIMIT = 5
 INITIAL_RETRY_DELAY = 2 
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             return {"list": []}
 
     api_request_interval = 20   
-    total_images_to_process = 7966  # adjust as needed
+    total_images_to_process = 5067  # adjust as needed
     delay_between_api_calls = 5
 
     for i in range(0, total_images_to_process, api_request_interval):
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         n = i + api_request_interval    
 
         # change your search query here
-        list_of_json_objects_batch = get_img_json("pelvis mri", m=m, n=n, vid=0)
+        list_of_json_objects_batch = get_img_json("shoulder xray", m=m, n=n, vid=0)
 
         if 'list' in list_of_json_objects_batch and list_of_json_objects_batch['list']:
             print(f"\nProcessing {len(list_of_json_objects_batch['list'])} images ({m}-{n})")
